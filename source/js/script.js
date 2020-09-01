2b86f84738714a752bfd0c423cd0743717163b8e
Masha <mariasoldatenko@mail.ru>
2020-09-02 01:03:36 +0300

    Добавлены интерактивные состояния элементов

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+var mainNav = document.querySelector (".main-nav");
+var navToggle = document.querySelector (".main-nav__toggle");
+
+mainNav.classList.remove("main-nav--nojs");
+navToggle.classList.add ("main-nav__toggle--opened");
+
+navToggle.addEventListener ("click", function() {
+  if (mainNav.classList.contains ("main-nav--closed")) {
+    mainNav.classList.remove ("main-nav--closed");
+    mainNav.classList.add ("main-nav--opened");
+    navToggle.classList.remove ("main-nav__toggle--closed");
+    navToggle.classList.add ("main-nav__toggle--opened");
+  } else {
+    mainNav.classList.add ("main-nav--closed");
+    mainNav.classList.remove ("main-nav--opened");
+    navToggle.classList.remove ("main-nav__toggle--opened");
+    navToggle.classList.add ("main-nav__toggle--closed");
+  }
+});
