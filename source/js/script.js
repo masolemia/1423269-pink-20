3ed283db8fa1a2d965a0d2e12557ebc9ed7e3487
Masha <mariasoldatenko@mail.ru>
2020-09-14 20:16:24 +0300

    Changes-2

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,8 @@ const mainNav = document.querySelector(".main-nav");
 const navToggle = mainNav.querySelector(".main-nav__toggle");
 
 mainNav.classList.remove("main-nav--nojs");
-navToggle.classList.add("main-nav__toggle--opened");
+mainNav.classList.add("main-nav--closed");
+navToggle.classList.add("main-nav__toggle--closed");
 
 navToggle.addEventListener("click", function(){
   if (mainNav.classList.contains("main-nav--closed")){
