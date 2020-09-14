e30a519a45d6f73301fa0293178605e7fa5834bf
Masha <mariasoldatenko@mail.ru>
2020-09-14 21:37:32 +0300

    Changes 4.1

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,7 @@ const mainNav = document.querySelector(".main-nav");
 const navToggle = mainNav.querySelector(".main-nav__toggle");
 
 mainNav.classList.remove("main-nav--nojs");
-mainNav.classList.add("main-nav--closed");
+mainNav.classList.add ("main-nav--closed");
 navToggle.classList.add("main-nav__toggle--closed");
 
 navToggle.addEventListener("click", function(){
@@ -19,3 +19,27 @@ navToggle.addEventListener("click", function(){
     navToggle.classList.add("main-nav__toggle--closed");
   }
 });
+
+
+const form = document.querySelector(".form");
+const successPopUp = form.querySelector(".pop-up--success");
+const closePopUp = form.querySelector(".pop-up__button");
+
+const onFormSubmit = function(evt) {
+  evt.preventDefault();
+  if (successPopUp.classList.contains("pop-up--closed")){
+    successPopUp.classList.remove("pop-up--closed");
+  }
+
+  closePopUp.addEventListener("click", onClosePopUpClick);
+  form.removeEventListener("submit", onFormSubmit);
+};
+
+const onClosePopUpClick = function() {
+  successPopUp.classList.add("pop-up--closed");
+
+  closePopUp.removeEventListener("click", onClosePopUpClick);
+  form.addEventListener("submit", onFormSubmit);
+};
+
+form.removeEventListener("submit", onFormSubmit);
